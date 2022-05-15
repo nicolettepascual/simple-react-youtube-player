1037165c36719eb4e8c062dae66844692e3b03e7
Nicolette <nicolette.pascual@gmail.com>
2022-05-15 18:55:19 +0800

    add styled components; revise YouTube container component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './css/App.css';
 import YoutubeContainer from "./components/YoutubeContainer";
+import styled from 'styled-components';
+
+const Grid = styled.div`
+  padding: 20px;
+`
+
+const Row = styled.div`
+    display: flex;
+`
+
+const Col = styled.div`
+    flex: ${(props) => props.size}
+`
 
 function App() {
   return (
     <div className="App">
-      <YoutubeContainer embedId="vg6Iq_Es3Wk" />
+      <Grid>
+        <Row>
+          <Col size={20}>
+            <YoutubeContainer embedId="Q2G53LuEUaU" />
+          </Col>
+          <Col size={4} style={{ border: '5px solid yellow' }}>Column</Col>
+        </Row>
+      </Grid>
     </div>
   );
 }
--- src/components/YoutubeContainer.js
+++ src/components/YoutubeContainer.js
@@ -1,21 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from 'styled-components';
 
-import "../css/YoutubeContainer.css"
+const YoutubeWrapper = styled.div`
+    position: relative;
+    padding-bottom: 56.25%;
+    overflow: hidden;
+    height: 0;
+    background-color: #8ea9c4;
+    border: 5px solid red;
+`
+
+const YoutubeIframe = styled.iframe`
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    padding: 20px;
+`
+
 
 const YoutubeContainer = ({ embedId }) => (
-    <div className="youtube-container-responsive">
-        <div className="youtube-container-responsive">
-            <iframe
-                src={`https://www.youtube.com/embed/${embedId}`}
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="Embedded youtube"
-            />
-        </div>
-
-    </div>
+    <YoutubeWrapper>
+        <YoutubeIframe
+            src={`https://www.youtube.com/embed/${embedId}`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="Embedded youtube"
+        />
+    </YoutubeWrapper>
 );
 
 YoutubeContainer.propTypes = {
