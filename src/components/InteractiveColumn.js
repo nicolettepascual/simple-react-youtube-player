3b931b5965559dec82a461d9b3c6be80df0fb49d
Nicolette <nicolette.pascual@gmail.com>
2022-05-15 23:26:00 +0800

    add clickable icon buttons; revise border of video ID text field

--- src/components/InteractiveColumn.js
+++ src/components/InteractiveColumn.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styled from 'styled-components';
+import { FaThumbsDown, FaThumbsUp } from "react-icons/fa";
 
 const StyledParentDiv = styled.div`
     padding: 0 30px 0 15px;
@@ -11,16 +12,28 @@ const StyledInput = styled.input`
     margin: 10px 0;
     padding: 10px 15px;
     border-radius: 6px;
+    border: 0;
 `
 const StyledChangeButton = styled.button`
     padding: 5px 15px;
     border-radius: 6px;
     border: 0;
     background-color: #8ea9c4;
+    width: 70%;
 `
 
 const StyledTextSpan = styled.span`
     color: white;
+    font-weight: bold;
+`
+
+const StyledIconBtnDiv = styled.div`
+    color: white;
+    font-size: 45px;
+    margin-bottom: 30px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
 `
 
 const InteractiveColumn = (props) => {
@@ -28,8 +41,12 @@ const InteractiveColumn = (props) => {
 
     return (
         <StyledParentDiv>
+            <StyledIconBtnDiv>
+                <FaThumbsUp onClick={() => alert("thumbs up clicked")} />
+                <FaThumbsDown onClick={() => alert("thumbs down clicked")} />
+            </StyledIconBtnDiv>
             <StyledTextSpan>Video ID</StyledTextSpan>
-            <StyledInput onChange={(e) => setVideoId(e.target.value)}/>
+            <StyledInput onChange={(e) => setVideoId(e.target.value)} />
             <StyledChangeButton onClick={() => props.onButtonPress(videoId)}>
                 Change
             </StyledChangeButton>
